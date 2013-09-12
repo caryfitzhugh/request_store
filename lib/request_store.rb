dbc78623a9dff2a44a5090ee998aeda42452e7f1
Cary FitzHugh <cary.fitzhugh@gmail.com>
2013-09-12 16:30:55 -0400

    STOP including middleware on your own.

--- lib/request_store.rb
+++ lib/request_store.rb
@@ -1,6 +1,7 @@
 require "request_store/version"
 require "request_store/middleware"
-require "request_store/railtie" if defined?(Rails::Railtie)
+# DONT' INCLUDE THIS!
+#require "request_store/railtie" if defined?(Rails::Railtie)
 
 module RequestStore
   def self.store
